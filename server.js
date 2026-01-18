1062944b633c94e7d66694ff07d6d0d85c2b2b43
Yashwant Bhosale <yashwantbhosale07@gmail.com>
2026-01-19 02:33:02 +0530

    init: basic server setup

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.use(express.json());
+
+app.get('/api/health', (req, res) => {
+    res.json({
+        success: true,
+        message: 'Modular Attachment Service API is running'
+    });
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Modular Attachment Service API');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
